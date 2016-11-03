23489e24132ccfadc4ac3efa59a876fd2bac5d0a
monster2003 <v.alex.4mail@gmail.com>
2016-11-03 16:50:36 +0300

    Вклад в банке составляет x рублей. Ежегодно он увеличивается на p процентов, после чего дробная часть копеек отбрасывается. Каждый год сумма вклада становится больше. Определите, через сколько лет вклад составит не менее y рублей.
    Входные данные
    Программа получает на вход три натуральных числа: x, p, y.
    
    Выходные данные
    Программа должна вывести одно целое число.

---
+++ P3063.pas
@@ -0,0 +1,21 @@
+(*
+ * Project: P3063
+ * User: Alex
+ * Date: 03.11.2016
+ *)
+program P3063;
+var
+    x,p,y,i,g:Integer ;
+begin
+    ReadLn(x);
+    ReadLn(p);
+    ReadLn(y);
+    i:=x;
+    g:=0;
+    while i<=y do
+    begin
+        g:=g+1;
+        i:=Trunc(i+(i/100*p));
+    end;
+    WriteLn(g);
+end.
